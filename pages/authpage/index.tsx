7aea99d450a5ba8b08758e368a859f1ed475fbd2
Jakub Mierzejek <gemixe1234@gmail.com>
2023-08-18 13:35:11 +0000

    changed some names and locations of components to me them more reusable

--- pages/authpage/index.tsx
+++ pages/authpage/index.tsx
@@ -5,102 +5,109 @@ import {
   Layout,
   VefskolinnLogo,
   InputForm,
-  LogInput,
   InputWrapper,
   InputLabel,
-  Btnwrapper,
+  ButtonWrapper,
 } from "../../styles/pageStyles/authpage.styles";
-import { FilledButton } from "@/components/buttons/filledButton";
-import { TextButton } from "@/components/buttons/textButton";
+import { TextButton, FilledButton } from "@/components/buttons";
+import { ShortInput } from "@/components/inputs";
 import AnimatedBackground from "@/components/animatedBackground";
 import { AnimatePresence } from "framer-motion";
 import login from "../api/auth/login";
 
-
 type CredentailsData = {
   email: string;
   password: string;
-}
+};
 
 const authPage = () => {
   const [authSwitch, setAuthSwitch] = useState(true);
-  const [credentials, setCredentials] = useLocalStorage<CredentailsData>("credentails", {email:"", password:""});
-  
-  const handleAuthSwitch : MouseEventHandler<HTMLButtonElement> = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const [credentials, setCredentials] = useLocalStorage<CredentailsData>(
+    "credentails",
+    { email: "", password: "" }
+  );
+
+  const handleAuthSwitch: MouseEventHandler<HTMLButtonElement> = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
     setAuthSwitch(!authSwitch);
   };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setCredentials({...credentials, [name]: value});
-  }
-  const login: MouseEventHandler<HTMLAnchorElement> = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {};
+    setCredentials({ ...credentials, [name]: value });
+  };
+  const login: MouseEventHandler<HTMLAnchorElement> = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {};
 
   return (
     <>
       <AnimatedBackground />
       <Layout>
-      <AnimatePresence>
-      
-        <Wrapper
-          layout
-          initial={{ opacity: 0, y:500}}
-          animate={{ opacity: 1, y:0}}
-          exit={{ opacity: 0 }}
-        >
-          <InputForm>
-            <VefskolinnLogo>{"{ Vefskolinn }"}</VefskolinnLogo>
-            {authSwitch ? (
-              <InputWrapper>
-                <InputLabel>Email</InputLabel>
-                <LogInput
-                  type="text"
-                  name="email"
-                  value={credentials.email}
-                  onChange={handleInputChange}
-                />
-                <InputLabel>Password</InputLabel>
-                <LogInput
-                  type="password"
-                  name="password"
-                  value={credentials.password}
-                  onChange={handleInputChange}
-                />
-              </InputWrapper>):(
-                 <InputWrapper>
-                 {/* Credentials  */}
-                   <InputLabel>Name</InputLabel>
-                   <LogInput />
-                   <InputLabel>Email</InputLabel>
-                   <LogInput />
-                   <InputLabel>Password</InputLabel>
-                   <LogInput />
-                   <InputLabel>Repeat Password</InputLabel>
-                 {/* About you  */}
-   
-                   <LogInput />
-                   <InputLabel>Background - what have you studied or worked with?</InputLabel>
-                   <LogInput />
-                   <InputLabel>Near future career goals</InputLabel>
-                   <LogInput />
-                   <InputLabel>Main interests</InputLabel>
-                   <LogInput />
-                   <InputLabel>Favourite band/s or artist/s</InputLabel>
-                   <LogInput />
-                 </InputWrapper>
+        <AnimatePresence>
+          <Wrapper
+            layout
+            initial={{ opacity: 0, y: 500 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0 }}
+          >
+            <InputForm>
+              <VefskolinnLogo>{"{ Vefskolinn }"}</VefskolinnLogo>
+              {authSwitch ? (
+                <InputWrapper>
+                  <InputLabel>Email</InputLabel>
+                  <ShortInput
+                    type="text"
+                    name="email"
+                    value={credentials.email}
+                    onChange={handleInputChange}
+                  />
+                  <InputLabel>Password</InputLabel>
+                  <ShortInput
+                    type="password"
+                    name="password"
+                    value={credentials.password}
+                    onChange={handleInputChange}
+                  />
+                </InputWrapper>
+              ) : (
+                <InputWrapper>
+                  {/* Credentials  */}
+                  <InputLabel>Name</InputLabel>
+                  <ShortInput />
+                  <InputLabel>Email</InputLabel>
+                  <ShortInput />
+                  <InputLabel>Password</InputLabel>
+                  <ShortInput />
+                  <InputLabel>Repeat Password</InputLabel>
+                  <ShortInput />
+
+                  {/* About you  */}
+                  <InputLabel>
+                    Background - what have you studied or worked with?
+                  </InputLabel>
+                  <ShortInput />
+                  <InputLabel>Near future career goals</InputLabel>
+                  <ShortInput />
+                  <InputLabel>Main interests</InputLabel>
+                  <ShortInput />
+                  <InputLabel>Favourite band/s or artist/s</InputLabel>
+                  <ShortInput />
+                </InputWrapper>
               )}
-              <Btnwrapper>
+              <ButtonWrapper>
                 <TextButton onClick={handleAuthSwitch}>
-                  {authSwitch?"Create Account":"I already have an account"}
+                  {authSwitch ? "Create Account" : "I already have an account"}
                 </TextButton>
-                <FilledButton onClick={login} href="/"> {authSwitch? "LOGIN":"REGISTER"} </FilledButton>
-              </Btnwrapper>
+                <FilledButton onClick={login} href="/">
+                  {" "}
+                  {authSwitch ? "LOGIN" : "REGISTER"}{" "}
+                </FilledButton>
+              </ButtonWrapper>
             </InputForm>
           </Wrapper>
-
-         
-       
         </AnimatePresence>
       </Layout>
     </>
