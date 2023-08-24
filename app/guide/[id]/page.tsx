a8e2c4b7cd9cd2f249e52c2b094bfb558664d021
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2023-08-24 23:02:02 +0000

    improved guides

--- app/guide/[id]/page.tsx
+++ app/guide/[id]/page.tsx
@@ -19,12 +19,13 @@ import {
   KnowledgeAndSkillsWrapper,
 } from "@/styles/pageStyles/guide0.styles";
 const getGuide = async (id: string) => {
+  if (!Types.ObjectId.isValid(id)) {
+    return null;
+  }
+  const objectId = new Types.ObjectId(id);
   await connectToDatabase();
-  const guide: GuideType | null = await G.findOne({ _id: id });
-  console.log(guide);
-
-  return guide;
-  
+  const guide: GuideType | null = await G.findOne({ _id: objectId });
+  return guide; 
 }
 const guide = async ({ params }: { params: { id: string } }) => {
   const g = await getGuide(params.id);
@@ -40,60 +41,29 @@ const guide = async ({ params }: { params: { id: string } }) => {
           <MainInfoWrapper>
             <GuideTitle>{g.title}</GuideTitle>
             <GuideSubtitle>Description</GuideSubtitle>
-            <GuideParagraph>
-              {g.description}
+            <GuideParagraph dangerouslySetInnerHTML={{ __html: g.description }}>
+             
             </GuideParagraph>
             <GuideSubtitle>Example</GuideSubtitle>
             <h1>{g.title}</h1>
-            <p>{g.description}</p>
+            <p dangerouslySetInnerHTML={{__html: g.description}}></p>
           </MainInfoWrapper>
 
           <SideOnfoWrapper>
             <SideFrame>
               <GuideSubtitle>Materials</GuideSubtitle>
-
-                  <MaterialLinks href="https://code.visualstudio.com/">How to set up VScode</MaterialLinks>
-
-                <MaterialLinks href="https://www.freecodecamp.org/news/introduction-to-git-and-github/">How to set up Git</MaterialLinks>
-
-                <MaterialLinks href="http://figma.com">Figma (the official page)</MaterialLinks>
-
-                <MaterialLinks href="https://www.codecademy.com/article/visual-studio-code">
-                    Getting started with HTML in VScode
-                    </MaterialLinks>
-
-                <MaterialLinks href="https://www.hostinger.com/tutorials/website/how-to-link-a-stylesheet-css-file-to-your-html-file">Basic CSS</MaterialLinks>
-
-                <MaterialLinks href="https://designlab.com/figma-101-course/introduction-to-figma/">Getting started with Figma</MaterialLinks>
-
+              {g.classes.map((material) => {
+                return (
+                  <MaterialLinks href={material.link}>
+                    {material.title}
+                  </MaterialLinks>
+                );
+              })}
             </SideFrame>
             <SideFrame>
               <GuideSubtitle>Topics</GuideSubtitle>
               <ul>
-                <li>
-                  <GuideParagraph>UI design</GuideParagraph>
-                </li>
-                <li>
-                  <GuideParagraph>HTML</GuideParagraph>
-                </li>
-                <li>
-                  <GuideParagraph>CSS</GuideParagraph>
-                </li>
-                <li>
-                  <GuideParagraph>installing software</GuideParagraph>
-                </li>
-                <li>
-                  <GuideParagraph>Git</GuideParagraph>
-                </li>
-                <li>
-                  <GuideParagraph>GitHub</GuideParagraph>
-                </li>
-                <li>
-                  <GuideParagraph>Terminal or shell</GuideParagraph>
-                </li>
-                <li>
-                  <GuideParagraph>IDE</GuideParagraph>
-                </li>
+                  return <li><GuideParagraph>{g.topicsList}</GuideParagraph></li>;
               </ul>
             </SideFrame>
             <SideFrame>
@@ -113,19 +83,17 @@ const guide = async ({ params }: { params: { id: string } }) => {
           <KnowledgeWrapper>
           <GuideParagraph>Knoweldge</GuideParagraph>
           <ul>
-            <li> <GuideParagraph>Getting to know and understand the system of Vefskólinn</GuideParagraph></li>
-            <li> <GuideParagraph>Understand what HTML is</GuideParagraph></li>
-            <li> <GuideParagraph>Understand what CSS is</GuideParagraph></li>
-            <li> <GuideParagraph>Understand what Git is</GuideParagraph></li>
-            <li> <GuideParagraph>Understand what Figma is</GuideParagraph></li>
+            {g.knowledge.map((knowledge) => {
+              return <li><GuideParagraph>{knowledge.knowledge}</GuideParagraph></li>;
+            })}
           </ul>
           </KnowledgeWrapper>
           <SkillsWrapper>
           <GuideParagraph>Skills</GuideParagraph>
           <ul>
-          <li><GuideParagraph>Be able to return guides on io.vefskoli.is</GuideParagraph></li>
-          <li><GuideParagraph>Be able to set up tools for web development</GuideParagraph></li>
-          <li><GuideParagraph>Be able to make a “hello world” website using those tools</GuideParagraph></li>
+            {g.skills.map((skill) => {
+              return <li><GuideParagraph>{skill.skill}</GuideParagraph></li>;
+            })}
           </ul>
           </SkillsWrapper>
           </KnowledgeAndSkillsWrapper>
--- models/guide.ts
+++ models/guide.ts
@@ -1,39 +1,84 @@
 import { Document, InferSchemaType, Schema, model, models } from "mongoose";
 
-const guideSchema = new Schema({
-  _id: { type: Schema.Types.String, required: true }, // we have Schema.Types for the schema but Types (from Mongoose) are for Mongoose documents
-  category: { type: Schema.Types.String, required: true },
-  references: { type: Schema.Types.Array, required: true },
-  uid: { type: Schema.Types.String, required: true },
-  title: { type: Schema.Types.String, required: true },
-  description: { type: Schema.Types.String, required: true },
-  knowledge: { type: Schema.Types.Array, required: true },
-  skills: { type: Schema.Types.Array, required: true },
-  resources: { type: Schema.Types.Array, required: true },
-  topics: { type: Schema.Types.Array, required: true },
+const guideReferenceSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  type: { type: Schema.Types.String, required: true },
+  name: { type: Schema.Types.String, required: true },
+  link: { type: Schema.Types.String, required: true },
   createdAt: { type: Schema.Types.Date, required: true },
   updatedAt: { type: Schema.Types.Date, required: true },
   __v: { type: Schema.Types.Number, required: true },
-  themeIdea: {
-    _id: { type: Schema.Types.ObjectId, required: true },
-    title: { type: Schema.Types.String, required: true },
-    description: { type: Schema.Types.String, required: true },
-    createdAt: { type: Schema.Types.Date, required: true },
-    updatedAt: { type: Schema.Types.Date, required: true },
-    __v: { type: Schema.Types.Number, required: true },
-    id: { type: Schema.Types.String, required: true },
-  },
+});
+
+const guideClassSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  title: { type: Schema.Types.String, required: true },
+  link: { type: Schema.Types.String, required: true },
+  createdAt: { type: Schema.Types.Date, required: true },
+  updatedAt: { type: Schema.Types.Date, required: true },
+  __v: { type: Schema.Types.Number, required: true },
+});
+
+const guideModuleSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  title: { type: Schema.Types.String, required: true },
+  uid: { type: Schema.Types.String, required: true },
+  createdAt: { type: Schema.Types.Date, required: true },
+  updatedAt: { type: Schema.Types.Date, required: true },
+  __v: { type: Schema.Types.Number, required: true },
+});
+
+const guideThemeIdeaSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  title: { type: Schema.Types.String, required: true },
+  description: { type: Schema.Types.String, required: true },
+  createdAt: { type: Schema.Types.Date, required: true },
+  updatedAt: { type: Schema.Types.Date, required: true },
+  __v: { type: Schema.Types.Number, required: true },
+});
+
+const guideKnowledgeSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  knowledge: { type: Schema.Types.String, required: true },
+  createdAt: { type: Schema.Types.Date, required: true },
+  updatedAt: { type: Schema.Types.Date, required: true },
+  __v: { type: Schema.Types.Number, required: true },
+});
+
+const guideSkillSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  skill: { type: Schema.Types.String, required: true },
+  createdAt: { type: Schema.Types.Date, required: true },
+  updatedAt: { type: Schema.Types.Date, required: true },
+  __v: { type: Schema.Types.Number, required: true },
+});
+
+const guideResourceSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  link: { type: Schema.Types.String, required: true },
+  description: { type: Schema.Types.String, required: true },
+  createdAt: { type: Schema.Types.Date, required: true },
+  updatedAt: { type: Schema.Types.Date, required: true },
+  __v: { type: Schema.Types.Number, required: true },
+});
+
+const guideSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true }, // we have Schema.Types for the schema but Types (from Mongoose) are for Mongoose documents
+  category: { type: Schema.Types.String, required: true },
+  references: { type: [guideReferenceSchema], required: true },
+  uid: { type: Schema.Types.String, required: true },
+  title: { type: Schema.Types.String, required: true },
+  description: { type: Schema.Types.String, required: true },
+  knowledge: { type: [guideKnowledgeSchema], required: true },
+  skills: { type: [guideSkillSchema], required: true },
+  resources: { type: [guideResourceSchema], required: true },
+  createdAt: { type: Schema.Types.Date, required: true },
+  updatedAt: { type: Schema.Types.Date, required: true },
+  __v: { type: Schema.Types.Number, required: true },
+  themeIdea: { type: guideThemeIdeaSchema, required: true },
   topicsList: { type: Schema.Types.String, required: true },
-  module: {
-    _id: { type: Schema.Types.ObjectId, required: true },
-    title: { type: Schema.Types.String, required: true },
-    uid: { type: Schema.Types.String, required: true },
-    createdAt: { type: Schema.Types.Date, required: true },
-    updatedAt: { type: Schema.Types.Date, required: true },
-    __v: { type: Schema.Types.Number, required: true },
-    id: { type: Schema.Types.String, required: true },
-  },
-  classes: { type: Schema.Types.Array, required: true },
+  module: { type: guideModuleSchema, required: true },
+  classes: { type: [guideClassSchema], required: true },
   id: { type: Schema.Types.String, required: true },
 });
 
