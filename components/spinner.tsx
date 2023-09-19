2e640c3f42073811711a63234099c1b15e38f884
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2023-09-19 16:30:58 +0000

    added spinner

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,12 +1,6 @@
 import "./spinner.css"
 
 const Spinner = () => {
-  const styles = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%,-50%)"
-  }
   return (
     <div className="loadingio-spinner-interwind-k6k2x9wyb5a"><div className="ldio-rqindywid1g">
 <div><div><div><div></div></div></div><div><div><div></div></div></div></div>
@@ -14,3 +8,5 @@ const Spinner = () => {
 
   );
 }
+
+export default Spinner;
