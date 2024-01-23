aa1c56e93f3d630a331770da034051f338db2199
rebekkarutm <rmdottir@gmail.com>
2024-01-23 10:07:26 +0000

    styles to fameCards added

--- components/fameCard/fameCard.tsx
+++ components/fameCard/fameCard.tsx
@@ -1,14 +1,56 @@
+'use client'
+
+import { useState } from "react";
 import { GuideType } from "@/models/guide";
 import React from "react";
+import {GuideCardContainer, CardInfo, Number, TitleWrapper, NumberWrapper, DefaultTitle, HoveredTitle} from "./styles"
+
 type Props = {
   guide: GuideType;
 };
+
 const FameCard = ({ guide }: Props) => {
+  const [isReviewHovered, setIsReviewHovered] = useState(false);
+  const [isReturnHovered, setIsReturnHovered] = useState(false)
+  const RetunrHandleMouseEnter = () => {
+    setIsReturnHovered(true);
+  };
+  const ReturnHandleMouseLeave = () => {
+    setIsReturnHovered(false);
+  };
+  const returnModifiedColor = isReturnHovered ? "brightness(80%)" : "brightness(100%)"
+
+  //Review hover state
+  const ReviewHandleMouseEnter = () => {
+    setIsReviewHovered(true);
+  };
+  const ReviewHandleMouseLeave = () => {
+    setIsReviewHovered(false);
+  };
+  const reviewModifiedColor = isReviewHovered ? "brightness(80%)" : "brightness(100%)"
+
   return (
-    <>
-      <h1>{guide.title}</h1>
-      <div>{guide.description}</div>
-    </>
+    <GuideCardContainer>
+      <CardInfo
+        style={{
+          backgroundPosition: 'center'
+          //backgroundImage:
+            // returnStatus.condition === !isUrl ? 'nota það' : returnStatus.backgroundImg
+        }}
+      >
+        <NumberWrapper>
+          <Number>{guide.title}</Number>
+        </NumberWrapper>
+        <TitleWrapper>
+          <DefaultTitle isShown={!isReturnHovered && !isReviewHovered}>
+            {guide.description}
+          </DefaultTitle>
+          <HoveredTitle isShown={isReturnHovered || isReviewHovered}>
+            Click to view
+          </HoveredTitle>
+        </TitleWrapper>
+      </CardInfo>
+    </GuideCardContainer>
   );
 };
 
---
+++ components/fameCard/styles.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+type TitleProps = {
+    isShown: boolean;
+  }
+
+export const GuideCardContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const CardInfo = styled.div`
+  box-shadow: 2px 4px 3px rgba(139, 139, 139, 0.25);
+  width: 24rem;
+  height: 20rem;
+  border-top-left-radius: 2rem;
+  border-top-right-radius: 2rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  background-position: center;
+`;
+
+export const Number = styled.h1`
+  font-size: 2.5rem;
+  font-family: "Poppins";
+  font-weight: 500;
+`;
+
+export const TitleWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const NumberWrapper = styled.div`
+  position: relative;
+  text-align: center;
+  padding: 2rem;
+`;
+
+export const Title = styled.h2<TitleProps>`
+  font-size: 1.5rem;
+  font-family: "Poppins";
+  transition: opacity 0.3s; // Add the transition effect
+  position: absolute;
+  text-align: center;
+  padding: 1rem;
+`;
+
+export const DefaultTitle = styled(Title)`
+  opacity: ${({ isShown }) => (isShown ? "1" : "0")};
+`
+export const HoveredTitle = styled(Title)`
+  opacity: ${({ isShown }) => (isShown ? "1" : "0")};
+`
