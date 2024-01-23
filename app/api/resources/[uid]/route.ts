0c929a147e380b3b837d881859c81831fd467afd
solal <solald@gmail.com>
2024-01-23 16:00:24 +0000

    fixing GET const in route.ts file, deleting unneccesary stuff

--- app/api/resources/[uid]/route.ts
+++ app/api/resources/[uid]/route.ts
@@ -1,23 +1,23 @@
 import { NextRequest, NextResponse as res} from "next/server";
 /*import type { NextApiRequest, NextApiResponse as res } from "next";*/
 import { connectToDatabase } from "@/utils/mongoose-connector";
-import { User, UserType } from "@/models/user";
-import { cookies } from "next/headers";
-import { unsealData, sealData} from "iron-session/edge";
+import resources from "@/app/resources/page";
 
 interface Success {
   message: string;
 }
 
 export const GET = async (
-    req: NextRequest,
+    req: NextRequest,  // NextRequest is something we'll use to filter our resources f.ex. when we are in a specific guide
   ) => {
     await connectToDatabase();
-    const guides = await Resources.find({});
-    if (guides === null) {
-      return res.json({ message: "Guide not found" }, { status: 404 });
+    const rescources = await Resources.find({});
+    if (resources === null) {
+      return res.json({ message: "Resource not found" }, { status: 404 });
     }
-     return res.json(guides, { status: 200 });
+     return res.json(resources, { status: 200 });
   }
   
-  //get 
+  //Need to clear the route, aka file structure, for the resources in API. 
+  //THe file in UID should be for the specific Module and Guide user has chosen. 
+  //And the route.ts file this is written in should be under resources, not UID. git 
