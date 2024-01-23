7c3c36eecf002f2921e5f18c274b0eeac5144ac6
maggarunars <margretrunars@gmail.com>
2024-01-23 12:15:32 +0000

    fixing some card styles

--- components/fameCard/styles.tsx
+++ components/fameCard/styles.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 type TitleProps = {
     isShown: boolean;
   }
@@ -12,19 +13,26 @@ export const GuideCardContainer = styled.div`
   margin-right: auto;
 `;
 
+export const BackgroundImage = styled.img`
+ background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="194" height="187" viewBox="0 0 194 187" fill="none" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M37.1025 187L52.865 117.859L0 71.3553L69.84 65.2039L97 0L124.16 65.2039L194 71.3553L141.135 117.859L156.897 187L97 150.338L37.1025 187Z" fill="black" fill-opacity="0.1"/></svg>');
+ background-image: cover;
+
+`
+
 export const CardInfo = styled.div`
-  box-shadow: 2px 4px 3px rgba(139, 139, 139, 0.25);
+  box-shadow: 2px 3px 3px 2px rgba(196, 186, 186, 0.25);
   width: 24rem;
   height: 20rem;
-  border-top-left-radius: 2rem;
-  border-top-right-radius: 2rem;
+  border-radius: 2rem;
   cursor: pointer;
   display: flex;
   justify-content: space-between;
   align-items: center;
   flex-direction: column;
   background-position: center;
-`;
+  background-color: rgb(162, 161, 243);
+
+   `
 
 export const Number = styled.h1`
   font-size: 2.5rem;
