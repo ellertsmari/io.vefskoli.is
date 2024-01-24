96def3409f9a71c8990ecb10f0434f0ef0eb082f
Marcela <slobodaniuc1994@gmail.com>
2024-01-24 16:56:51 +0000

    not finished styled page

--- app/resources/styles.tsx
+++ app/resources/styles.tsx
@@ -2,13 +2,19 @@
 //THIS FILE needs to be reworked for the purpose of the Resources container. 
 //I've removed some stuff we didn't need. 
 
+type Props={
+ image?: string;
+}
+
 
 'use client';
 import styled from "styled-components"
 import { motion } from "framer-motion"
+import { type } from "os";
 
 export const Title = styled.h1`
     font-style: Poppins;
+    size:24px;
 `
 
 export const ModuleTitle = styled.h1`
@@ -47,3 +53,20 @@ export const DropdownContainer = styled.div`
     font-weight: 500;
     margin: 0;
     `
+    export const VideoCard  = styled.div <Props>`
+    width: 250px;
+    height: 250px;
+    background-color: #D1D1D1;
+    border-radius: 20px;
+    background-image: url(${props => props.image || 'https://www.agilealliance.org/wp-content/uploads/2022/09/error-404-search.png'});
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    text-align: center;
+    `
+    export const VideoCardText = styled.a`
+    font-size: 1.5rem;
+
+    `
+    
