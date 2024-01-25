8599992425d112558ea148bdf44f3888b96dbf4a
Péter Takács <petertakacs@Peters-MacBook-Pro.local>
2024-01-25 14:45:32 +0000

    Managed to keep the token updated and hid sensitive lines of code.

--- app/api/zoomapi/route.ts
+++ app/api/zoomapi/route.ts
@@ -1,16 +1,42 @@
 
 
-export const GET= async ()=>{
+  let token = ""
+const getVideos= async(token:string)=>{
   const url = "https://api.zoom.us/v2/users/vefskolinn@tskoli.is/recordings?page_size=30&from=2021-10-11"
+  
   const response = await fetch (url,{
       headers: {
-          authorization: 'Bearer eyJzdiI6IjAwMDAwMSIsImFsZyI6IkhTNTEyIiwidiI6IjIuMCIsImtpZCI6IjdkMTVlNTYyLTg1MTctNGZmMi05YWE1LWJjN2E0MDJiMTEwZiJ9.eyJhdWQiOiJodHRwczovL29hdXRoLnpvb20udXMiLCJ1aWQiOiI5T3VlNWJJSFNNQ3lUUlB5M3AxSGtBIiwidmVyIjo5LCJhdWlkIjoiYTdjNzdlY2EyNGM1NTEyNDVhOTA3ZDc2NjI4ZTMyOTAiLCJuYmYiOjE3MDYxMDcxNjgsImNvZGUiOiJuNUtqWGVmVVNhdVhiLUZvaTJyUmd3cWJCeWpLeXBwRmgiLCJpc3MiOiJ6bTpjaWQ6eUZEUU9KaXdTNjZtaklfaWY1SUltdyIsImdubyI6MCwiZXhwIjoxNzA2MTEwNzY4LCJ0eXBlIjozLCJpYXQiOjE3MDYxMDcxNjgsImFpZCI6InhUbXdWYk5kUVJHdjVYQkl1eXZJMkEifQ.5towcupypt664ZoyuqQ2T6TACI__D24Dvf1oPjF2vzoNe4ogSQ21HwnK12VNJRzqZlNdZSHeGDUhq3kkpcvetQ'
+          authorization: "Bearer "+ token
       }
   })
- 
-    const data=await response.json()
+  return response.json()
+}
+
+
+export const GET= async ()=>{
+  let data = await getVideos(token)
+  if (data.code===124){
+  const tokenResponse = await fetch ("https://zoom.us/oauth/token?grant_type=account_credentials&account_id=xTmwVbNdQRGv5XBIuyvI2A",{
+      method: "POST",
+      headers:{
+        authorization: process.env.BASIC_AUTH
+        
+      }
+  })  
+  const tokenData = await tokenResponse.json() 
+  token = tokenData.access_token
+  console.log (token)
+  data = await getVideos(token)
+  console.log (data)
+  }
+   
   return Response.json(data)
   }
+
+
+
+
+
 
   
 
