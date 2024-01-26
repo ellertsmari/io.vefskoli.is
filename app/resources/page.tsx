97ea0f9adcf1736bd4c7f7e52e51020507541492
solal <solald@gmail.com>
2024-01-26 13:59:49 +0000

    Created code that works for fetching the zoom videos from cloud, using a new dropdown menu component to handle that

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -5,27 +5,26 @@ import { VideoCardText, Title,TopContainer, GuidesContainer, DropdownContainer, 
 import { FilledButton } from "@/components/buttons";
 import Modal from "@/components/modal/modal"
 import DropdownResources from "@/components/dropDown/dropDownResource";
-import { getVideos } from "../api/zoomapi/route";
 
-
-// need to define user
-// need to get title for recordings
-// need to define data
-
-
-//This is a serverside component that mostly handles data fetching and passing it to the Resources component
 const resources = async () => {
   const user: OmitPassword | string = await useServerUser();
-  const resources: any[] | undefined | null = await getVideos(user);
-  if (!resources) return <>No resources found</>;
-  console.log(resources);
+  if (!user) return <>Please login</>
 
-  const data = await getVideos(user);
-  if (!data || !data.recordings) return <>No resources found</>;
+  const response = await fetch("http://localhost:3000/api/zoomapi");  //this should eventually change to the io.vefskoli.is 
+  const data = await response.json();
+  if (!data || !data.meetings) return <>No resources found</>;
   console.log(data);
 
   // Define options for the dropdown. THis is assuming the recordings are an array. If it's not we have to structure differently. 
-  const options = data.recordings.map(recording => recording.title.substring(0, 8));  // should filter by fyrst 8 digits. 
+  const options = data.meetings.map(recording => recording.topic.substring(0, 8));  // should filter by fyrst 8 digits. 
+
+
+/*Notes for what needs to be done so that the dropdownmenu works as it should. 
+Gera object med key og property fyrir thad sem vid thurfum. key 8 stafir then property should be "this sentence"
+that will then appear in the dropdown menu 
+Also have to make it so that they only show one instance of eache 8 letters so the dropdown menu isn't 
+repeating everything
+*/
 
   return (
     <>
@@ -38,7 +37,7 @@ const resources = async () => {
       </TopContainer>
         <Title>Videos and Recordings</Title>
         <FilledButton>Drive</FilledButton>
-        <GuidesContainer> {resources.map(resource => {
+        <GuidesContainer> {data.meetings.map(resource => {
           return (
             <VideoCardText> 
               <Modal/> 
--- components/dropDown/dropDownResource.tsx
+++ components/dropDown/dropDownResource.tsx
@@ -1,10 +1,9 @@
 'use client'
 
+//This is a new dropdown component created specifically for the Resources page to handle the client side for the dropdown function.
+
 import React, { useState } from "react";
 import Dropdown from "./dropDown";
-
-
-
 
 const DropdownResources = ({ options }) => {
   const [selected, setSelected] = useState(options[0]); // initializes selected state to the first option
