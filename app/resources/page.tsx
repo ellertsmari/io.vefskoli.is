4fc7aa7b25bfa7a39cfa8630d4271762378c899b
PilaPina <solald@gmail.com>
2024-01-27 20:21:47 +0000

    Used Key-Value pairs in object to map the topics and display correct names in the dropdown

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -15,8 +15,22 @@ const data = await response.json();
 if (!data || !data.meetings) return <>No resources found</>;
 console.log(data);
 
+//We filter the videos with the first 8 digits in their title (topic). 
+//To make sure the dropdown menu doesn't show only the first 8 digits we use key and property in an object. 
+const nameDrop = {
+  'Module 1' : 'MODULE 1',
+  'Module 2' : 'Module 2',
+  'Module 3' : 'Module 3',
+  'Module 4' : 'Module 4',
+  'Module 5' : 'Module 5',
+  'Module 6' : 'Module 6',
+  'Las palm' : 'Las Palmas',
+  'Introduc' : 'Introduction',
+};
+
 // Define options for the dropdown. Set is used to ensure that only unique values are present in optionsSet
-const optionsSet = new Set(data.meetings.map(recording => recording.topic.substring(0, 8)));  // should filter by first 8 digits. 
+// This filters by the first 8 digits. 
+const optionsSet = new Set(data.meetings.map(recording => nameDrop[recording.topic.substring(0, 8)] || recording.topic.substring(0, 8)));  
 const options = Array.from(optionsSet);   //Array.from() is used to convert the Set back into an array which can be used as the options for dropdown. This will ensure that each module only appears once in the dropdown, regardless of how many videos it has.
 
   return (
