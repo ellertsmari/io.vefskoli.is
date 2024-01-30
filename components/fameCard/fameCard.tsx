2dd50f1db0c02045e86dc9d93ee9861ac79dcd81
rebekkarutm <rmdottir@gmail.com>
2024-01-30 10:19:30 +0000

    comments added to fameCard.tsx

--- components/fameCard/fameCard.tsx
+++ components/fameCard/fameCard.tsx
@@ -2,6 +2,7 @@
 
 "use client";
 
+// importing necessary libraries and types
 import { useState } from "react";
 import { GuideType } from "@/models/guide";
 import { ReturnType } from "@/models/return";
@@ -12,7 +13,6 @@ import {
   Title,
   TitleWrapper,
   DescriptionWrapper,
-  DefaultDescription,
   HoveredDescription,
   PencilEdit,
   CloseX,
@@ -20,50 +20,63 @@ import {
   ImgStyle,
 } from "./styles";
 import Edit from "./editCard";
+
+// defining the type of the props that the FameCard component will receive
 type Props = {
   guide: GuideType;
   returnData: ReturnType;
 };
 
+// FameCard component definition
 const FameCard = ({ guide, returnData }: Props) => {
-
-  //State variables used for hover states over the Fame cards. 
-  //When you hover over the Fame card, 'click to view' appears as well as a pencil icon
-  //When the pencil icon is clicked, the modal opens which allows you to 1.change the title of the guide 2. change the picture 3. remove guide from hall of fame
+  // state variables for handling hover and modal open/close
   const [isReturnHovered, setIsReturnHovered] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
+
+  // function to handle opening and closing of the modal
+  // when the pencil icon is clicked, the modal opens which allows you to 1. change the title of the guide 2. change the picture 3. remove guide from hall of fame
   const handleOpenModal = () => {
     setModalOpen(!modalOpen);
   };
+
+  // function to handle mouse enter and leavve events
+  // when you hover over the Fame card, 'click to view' appears as well as a pencil icon
   const ReturnHandleMouseEnter = () => {
     setIsReturnHovered(true);
   };
   const ReturnHandleMouseLeave = () => {
     setIsReturnHovered(false);
   };
-    
+  
+  // rendering the component
   return (
     <>
       <GuideCardContainer>
         <CardInfo
+          // hover effect, click to view and pencil icon appears
           onMouseEnter={ReturnHandleMouseEnter}
           onMouseLeave={ReturnHandleMouseLeave}
         >
           <TitleWrapper>
+            {/* displaying the name the student chose for their project */}
             <Title>{returnData.projectName}</Title>
           </TitleWrapper>
+          {/* displaying the background image the student chose with pictureUrl when handing in project */}
           <ImgStyle img={returnData.pictureUrl}>
             <DescriptionWrapper>
+              {/* if hovering, the click to view nad pencil icon is displayed */}
               <HoveredDescription isShown={isReturnHovered}>
                 Click to view
               </HoveredDescription>
             </DescriptionWrapper>
+            {/* if hovering, pencil icon appears and if clicked, the modal opens */}
             {isReturnHovered && (
               <PencilEdit onClick={handleOpenModal}>✏️</PencilEdit>
             )}
           </ImgStyle>
         </CardInfo>
       </GuideCardContainer>
+      {/* modal opens and an overlay is displayed over the page */}
       {modalOpen && (
         <>
           <Overlay onClick={handleOpenModal}></Overlay>
@@ -80,8 +93,11 @@ const FameCard = ({ guide, returnData }: Props) => {
               zIndex: "20",
             }}
           >
+            {/* onClick of the x, the modal closes */}
             <CloseX onClick={handleOpenModal}>X</CloseX>
             <Edit
+              // options for editing, the id is the return id, projectName and pictureUrl are initially empty
+              // vote cannot be changed here
               returns={{
                 _id: returnData._id,
                 projectName: "",
