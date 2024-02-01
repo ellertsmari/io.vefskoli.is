4a9024e372f217df52c2ed4b2bf960922a160322
vikkiklikk <viktor.birgisson@gmail.com>
2024-02-01 13:35:36 +0000

    updated some styling to test

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,24 @@
+"use client";
+
+import { ModuleTitle, TopContainer } from "@/components/guides/styles";
+import { MainContent } from "@/components/mainLayout";
+import { Title } from "@/components/sidebar/sidebar.style";
+import { MainContainer } from "@/styles/pageStyles/review.styles";
+
 const Blog = () => {
   return (
-    <div>
-      <h1>blog test</h1>
-    </div>
+    <>
+      <MainContent>
+        <TopContainer>
+          <ModuleTitle>
+            <h1>Blog</h1>
+          </ModuleTitle>
+        </TopContainer>
+        <Title>
+          <h1>title</h1>
+        </Title>
+      </MainContent>
+    </>
   );
 };
 
