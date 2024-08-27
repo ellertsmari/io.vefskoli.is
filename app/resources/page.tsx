081bf021c3eb8abf27031e41bc4ee3c5eb4d303e
Smári <ellertsmari@gmail.com>
2024-08-27 11:34:31 +0000

    added revalidation to the zoom videos on 10 minutes

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -3,14 +3,14 @@ import { OmitPassword } from "@/utils/types/types";
 import Recordings from "@/components/resources/resources";
 
 //This page fetches resources from an API and renders the Recordings component with the fetched data.
-
+export const revalidate = 600;
 const resources = async () => {
 const user: OmitPassword | string = await useServerUser();
 // If there is no user, it returns a message asking the user to log in.
 if (!user) return <>Please login</>
 
 // This fetches data from the Zoom API.
-const response = await fetch("https://io.vefskoli.is/api/zoomapi");  //this should eventually change to io.vefskoli.is // tip from coder: use env URL for this or the production URL
+const response = await fetch("https://io.vefskoli.is/api/zoomapi", );  //this should eventually change to io.vefskoli.is // tip from coder: use env URL for this or the production URL
 
 // This parses the response data as JSON.
 const data = await response.json();
